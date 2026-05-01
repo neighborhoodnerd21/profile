c9530efa99ec37b0b9b514f551d0a7bff1555d19
NeighborhoodNerd21 <neighborhoodnerd21@gmail.com>
2026-05-01 18:40:49 -0400

    working on devscripts

--- devScripts/preCommit.mjs
+++ devScripts/preCommit.mjs
@@ -5,5 +5,4 @@ console.log(htmlSuccess.stdout);
 const cssSuccess = await $`zx devScripts/validateCss.mjs`;
 console.log(cssSuccess.stdout);
 
-console.log("All files validated and ready for commit.");
 process.exit(0);
