bf6b774fe6ce58a2a24874085a34f205e6028b43
NeighborhoodNerd21 <neighborhoodnerd21@gmail.com>
2026-04-30 20:19:00 -0400

    working on devscripts

--- devScripts/preSessionCheck.mjs
+++ devScripts/preSessionCheck.mjs
@@ -19,3 +19,12 @@
 // check origin state to make sure we are not behind or ahead of origin, and that there are no uncommitted changes.
 // If there are uncommitted changes, stash them and pop the stash after the session is done?
 // If we are behind or ahead of origin, print a warning and exit.
+
+const gitStatus = await $`git status --porcelain`;
+
+if (gitStatus === 0) {
+  console.log("No uncommitted changes.");
+} else {
+  console.log("There are uncommitted changes.");
+  process.exit(1);
+}
